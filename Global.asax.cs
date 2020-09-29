a517494dc4be6d8caafc253a5b33399cd0b2b97e
microphione <65678508+microphione@users.noreply.github.com>
2020-09-29 09:53:51 +0200

    Add files via upload

---
+++ Global.asax.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using System.Web.Security;
+
+namespace ListaZakupow
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        // Dodane by nadać role zalogowanemu użytkownikowi
+        protected void Application_PostAuthenticateRequest(object sender, EventArgs e)
+        {
+            if (HttpContext.Current.User.Identity.IsAuthenticated)
+            {
+                FormsIdentity formsIdentity = (FormsIdentity)HttpContext.Current.User.Identity;
+                HttpContext.Current.User =
+                    new System.Security.Principal.GenericPrincipal(HttpContext.Current.User.Identity, formsIdentity.Ticket.UserData.Split('|'));
+            }
+        }
+    }
+}
---
+++ ZnajdzUzyt.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Core.Objects;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Web;
+using ListaZakupow;
+namespace ListaZakupow
+{
+
+    public partial class ListaZakupowEntities
+    {
+        [DbFunction("EntityModels.Store", "ZnajdzUzyt")]
+        public string ZnajdzUzyt(string login, string haslo)
+        {
+            var objectContext = ((IObjectContextAdapter)this).ObjectContext;
+
+            var parameters = new List<ObjectParameter>();
+            parameters.Add(new ObjectParameter("Login", login));
+            parameters.Add(new ObjectParameter("Haslo", haslo));
+
+            return objectContext.CreateQuery<string>("ListaZakupowModel.Store.ZnajdzUzyt(@Login, @Haslo)", parameters.ToArray())
+                 .Execute(MergeOption.NoTracking)
+                 .FirstOrDefault();
+        }
+    }
+}
